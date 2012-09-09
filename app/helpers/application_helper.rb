075efaad85d34a093dbf5a082e97e22e4afd5e5d
Ryan Townsend <ryan@ryantownsend.co.uk>
2012-09-09 13:35:51 +0100

    Added PJAX layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def set_title(title)
+    content_for(:title, title)
+  end
+  
+  def title_tag
+    content_tag(:title) do
+      content_for?(:title) ? content_for(:title) : "PJAX RequireJS Test"
+    end
+  end
 end
