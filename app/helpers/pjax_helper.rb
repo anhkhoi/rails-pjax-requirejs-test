7b1bb64c5c8e983032b6da2d0042155bb5426590
Ryan Townsend <ryan@ryantownsend.co.uk>
2012-09-26 17:02:05 +0100

    Added conditional PJAX container

--- app/helpers/pjax_helper.rb
+++ app/helpers/pjax_helper.rb
@@ -2,4 +2,14 @@ module PjaxHelper
   def pjax_container(name, &block)
     content_tag(:div, "data-pjax-container" => name, &block)
   end
+  
+  def conditional_pjax_container(name, &block)
+    if pjax_wants?(name)
+      pjax_container(name, &block)
+    end
+  end
+  
+  def pjax_wants?(name)
+    true
+  end
 end
