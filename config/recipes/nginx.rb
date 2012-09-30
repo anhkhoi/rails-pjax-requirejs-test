b262d59cfa906c6d498a3c623bd5445391633581
Ryan Townsend <ryan@ryantownsend.co.uk>
2012-09-30 23:02:04 +0100

    Added custom NGINX monit config script

--- config/recipes/nginx.rb
+++ config/recipes/nginx.rb
@@ -31,10 +31,14 @@ Capistrano::Configuration.instance(:must_exist).load do |config|
     task :configure_monit, roles: nginx_roles do
       # upload the config file
       upload_monit_config("nginx", {
+        template: "nginx.erb",
         process_name: "nginx",
         pid_file: nginx_pid_file,
         start_command: "/etc/init.d/nginx start",
-        stop_command: "/etc/init.d/nginx stop"
+        stop_command: "/etc/init.d/nginx stop",
+        max_children: 250,
+        max_restarts: 5,
+        max_cycles: 5
       })
       # ensure the syntax is valid
       monit.validate_syntax
