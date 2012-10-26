2ab51576ab3055d00d5fe0e36dae417e9c4c161b
Ryan Townsend <ryan@ryantownsend.co.uk>
2012-10-26 13:42:26 +0100

    Updated config

--- config/deploy/recipes/puma.rb
+++ config/deploy/recipes/puma.rb
@@ -7,8 +7,8 @@ Capistrano::Configuration.instance(:must_exist).load do |config|
   # settings
   _cset(:puma_roles) { :app }
   _cset(:puma_port) { 8080 }
-  _cset(:puma_pid_file) { File.join(shared_path, "tmp/pids/puma.pid") }
-  _cset(:puma_state_file) { File.join(shared_path, "tmp/pids/puma.state") }
+  _cset(:puma_pid_file) { File.join(shared_path, "pids/puma.pid") }
+  _cset(:puma_state_file) { File.join(shared_path, "pids/puma.state") }
   _cset(:puma_log_file) { File.join(shared_path, "log/puma.log") }
   _cset(:puma_shell) { capture("which rvm-shell").chomp }
   _cset(:puma_service_name) { "puma" }
@@ -20,7 +20,7 @@ Capistrano::Configuration.instance(:must_exist).load do |config|
       upload_monit_config(fetch(:puma_service_name), {
         process_name: fetch(:puma_service_name),
         pid_file: fetch(:puma_pid_file),
-        start_command: "#{fetch :puma_shell} -c 'cd #{current_path} ; $rvm_path/bin/rvm rvmrc trust load ; nohup bundle exec puma -e #{rails_env} -S #{fetch :puma_state_file} --port=#{fetch :puma_port} --pidfile=#{fetch :puma_pid_file} >> #{fetch :puma_log_file} 2>&1 &'",
+        start_command: "#{fetch :puma_shell} -c 'cd #{current_path} ; $rvm_path/bin/rvm rvmrc trust load ; nohup bundle exec puma -e production -S #{fetch :puma_state_file} --port=#{fetch :puma_port} --pidfile=#{fetch :puma_pid_file} >> #{fetch :puma_log_file} 2>&1 &'",
         stop_command: "#{fetch :puma_shell} -c 'if [ -d #{current_path} ] && [ -f #{fetch :puma_pid_file} ]; then cd #{current_path} ; pumactl --state #{fetch :puma_state_file} stop ; fi ; rm -f #{fetch :puma_pid_file} #{fetch :puma_state_file}'",
         restart_command: "#{fetch :puma_shell} pumactl --state #{fetch :puma_state_file} restart"
       })
--- config/deploy/recipes/sidekiq.rb
+++ config/deploy/recipes/sidekiq.rb
@@ -6,7 +6,7 @@ Capistrano::Configuration.instance(:must_exist).load do |config|
   
   # settings
   _cset(:sidekiq_roles) { :app }
-  _cset(:sidekiq_pid_file) { File.join(shared_path, "tmp/pids/sidekiq.pid") }
+  _cset(:sidekiq_pid_file) { File.join(shared_path, "pids/sidekiq.pid") }
   _cset(:sidekiq_config_file) { File.join(current_path, "config/sidekiq.yml") }
   _cset(:sidekiq_log_file) { File.join(shared_path, "log/sidekiq.log") }
   _cset(:sidekiq_shell) { capture("which rvm-shell").chomp }
@@ -19,7 +19,7 @@ Capistrano::Configuration.instance(:must_exist).load do |config|
       upload_monit_config(fetch(:sidekiq_service_name), {
         process_name: fetch(:sidekiq_service_name),
         pid_file: fetch(:sidekiq_pid_file),
-        start_command: "#{fetch :sidekiq_shell} -c 'cd #{current_path} ; $rvm_path/bin/rvm rvmrc trust load ; nohup bundle exec sidekiq -e #{rails_env} -C #{fetch :sidekiq_config_file} -P #{fetch :sidekiq_pid_file} >> #{fetch :sidekiq_log_file} 2>&1 &'",
+        start_command: "#{fetch :sidekiq_shell} -c 'cd #{current_path} ; $rvm_path/bin/rvm rvmrc trust load ; nohup bundle exec sidekiq -e production -C #{fetch :sidekiq_config_file} -P #{fetch :sidekiq_pid_file} >> #{fetch :sidekiq_log_file} 2>&1 &'",
         stop_command: "#{fetch :sidekiq_shell} -c 'if [ -d #{current_path} ] && [ -f #{fetch :sidekiq_pid_file} ]; then cd #{current_path} ; bundle exec sidekiqctl stop #{fetch :sidekiq_pid_file}; fi'"
       })
       # note: can also use `su - myuser -c 'COMMAND'` to run via RVM shell 
