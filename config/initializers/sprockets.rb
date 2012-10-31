65f092d73af7cc14631a1171720046e34c87777b
Ryan Townsend <ryan@ryantownsend.co.uk>
2012-10-31 19:30:54 +0000

    Inspect Sprockets [2]

--- config/initializers/sprockets.rb
+++ config/initializers/sprockets.rb
@@ -5,17 +5,23 @@ class JavascriptIncludeProcessor < Sprockets::Processor
       javascripts = {}
       # read all the assets
       assets.each_file do |path|
-        puts path
+        puts "one"
         attributes = assets.attributes_for(path)
+        puts "two"
         # if it's a javascript file
         if attributes.content_type == "application/javascript"
+          puts "three"
           # load the requirement path
           basic_path = attributes.logical_path.gsub(/#{attributes.format_extension}$/, "")
+          puts "four"
           # calculate the full path
           full_path = context.asset_path("#{basic_path}#{attributes.format_extension}")
+          puts "five"
           full_path = full_path.gsub(/#{attributes.format_extension}$/, "")
+          puts "six"
           # merge into the hash
           javascripts.merge!({ basic_path => full_path })
+          puts "seven"
         end
       end
       # output as a JSON hash
